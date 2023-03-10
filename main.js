cbbd18e836d11a38d49b7ce11e20d7e1e8834cd8
Rods <rodolfo.perez01@iest.edu.mx>
2023-03-10 15:36:52 -0600

    Avances

---
+++ main.js
@@ -0,0 +1,39 @@
+let inputsFields = document.querySelectorAll("#person-form input");
+let bottomSave = document.querySelector("#save-button");
+let arrayPeople = [];
+let data = {};
+
+inputsFields.forEach((fieldsData) => {
+  fieldsData.addEventListener("keyup", (event) => {
+    let property = event.target.name;
+    let value = event.target.value;
+    data[property] = value;
+  });
+});
+bottomSave.addEventListener("click", () => {
+  arrayPeople.push(data);
+  console.log(arrayPeople);
+});
+
+arrayPeople.forEach((person) => {
+  let { nombre, Apellido, correo } = person;
+
+  let peopleRow = document.createElement("tr");
+
+  let tdName = document.createElement("td");
+  let tdLastName = document.createElement("td");
+  let tdEmail = document.createElement("td");
+  let eraseBottom = document.createElement("buttom");
+
+  let nameInfo = document.createTextNode(nombre);
+  let lastNameInfo = document.createTextNode(Apellido);
+  let emailInfo = document.createTextNode(correo);
+
+  tdName.appendChild(nameInfo);
+  tdLastName.appendChild(lastNameInfo);
+  tdEmail.appendChild(emailInfo);
+
+  peopleRow.append(tdName, tdLastName, tdEmail, eraseBottom);
+
+  document.getElementById("people-wrapper").appendChild(peopleRow);
+});
