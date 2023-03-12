216213801efe644b7fe11f594e046e38a3e9762c
root <root@Damic-PC>
2023-03-12 14:29:52 -0600

    Botón de borrado

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let inputsFields = document.querySelectorAll("#person-form input");
 let bottomSave = document.querySelector("#save-button");
 let arrayPeople = [];
 let data = {};
+let counter = 1;
 
 inputsFields.forEach((fieldsData) => {
   fieldsData.addEventListener("keyup", (event) => {
@@ -10,19 +11,27 @@ inputsFields.forEach((fieldsData) => {
     data[property] = value;
   });
 });
+
 bottomSave.addEventListener("click", () => {
   arrayPeople.push(data);
   let table = document.getElementById("people-wrapper");
   table.innerHTML = "";
   arrayPeople.forEach((person) => {
     let { nombre, Apellido, correo } = person;
-
+    let idLoc = counter;
     let peopleRow = document.createElement("tr");
+    peopleRow.id = "row-" + idLoc;
 
     let tdName = document.createElement("td");
     let tdLastName = document.createElement("td");
     let tdEmail = document.createElement("td");
-    let eraseBottom = document.createElement(`button`);
+    let eraseButton = document.createElement("button");
+    eraseButton.type = "button";
+    eraseButton.value = "delete";
+    eraseButton.innerText = "Eliminar";
+    eraseButton.onclick = () => {
+      document.getElementById("row-" + idLoc).remove();
+    }
 
     let nameInfo = document.createTextNode(nombre);
     let lastNameInfo = document.createTextNode(Apellido);
@@ -32,10 +41,12 @@ bottomSave.addEventListener("click", () => {
     tdLastName.appendChild(lastNameInfo);
     tdEmail.appendChild(emailInfo);
 
-    peopleRow.append(tdName, tdLastName, tdEmail, eraseBottom);
+    peopleRow.append(tdName, tdLastName, tdEmail, eraseButton);
 
     document.getElementById("people-wrapper").appendChild(peopleRow);
+    counter++;
   });
 
   data = {};
 });
+
