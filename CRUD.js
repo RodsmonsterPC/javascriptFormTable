b19de1dea314376ec8872c9301b4b8ad6fe6ac9b
Rods <rodolfo.perez01@iest.edu.mx>
2023-03-15 14:10:28 -0600

    CRUD crear

---
+++ CRUD.js
@@ -0,0 +1,43 @@
+/* https://github.com/Israel2709/24js */
+let inputsFields = document.querySelectorAll("#person-form input");
+let bottomSave = document.querySelector("#save-button");
+let data = {};
+
+const getKoders = async () => {
+  let response = await fetch(
+    "https://kodemia-24g-default-rtdb.firebaseio.com/Rodolfo/koders/.json"
+  );
+  let data = await response.json();
+  return data;
+};
+
+const createKoder = async (koderObject) => {
+  let response = await fetch(
+    "https://kodemia-24g-default-rtdb.firebaseio.com/Rodolfo/koders/.json",
+    {
+      method: "POST",
+      body: JSON.stringify(koderObject),
+    }
+  );
+  let data = await response.json();
+
+  return data;
+};
+const resetForm = () => {
+  document.querySelectorAll(`#person-form input`).forEach((element) => {
+    element.value = "";
+  });
+};
+
+inputsFields.forEach((fieldsData) => {
+  fieldsData.addEventListener("keyup", (event) => {
+    let property = event.target.name;
+    let value = event.target.value;
+    data[property] = value;
+  });
+});
+
+bottomSave.addEventListener("click", () => {
+  createKoder(data);
+  resetForm();
+});
