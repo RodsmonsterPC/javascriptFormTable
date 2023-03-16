06a2037958e74fdb804dc520d73014d01e629ec9
JesusMendoza815 <jesuscamposmendoza815@gmail.com>
2023-03-16 00:18:22 -0600

    feat: render users

--- Cards/fetch.js
+++ Cards/fetch.js
@@ -1,1 +1,14 @@
-import { getUser, getFriends, deleteFriend, createFriend } from "./api";
+import { getUser, getFriends, deleteFriend, createFriend } from "./api.js";
+
+const renderUsers = ( async() => {
+  const response = await getUser();
+  const usersList = response['results'];
+  for (const key in usersList) {
+    let { gender, name, dob, picture, nat} = usersList[key];
+    console.log(`${gender} ${name.first} ${dob.date} ${picture.large} ${nat}`);
+    //función para crear tarjetas 
+    //ejemplo
+    //createCards(gender, name.first, dob.date, picture.large, nat)
+  }
+});
+renderUsers();
